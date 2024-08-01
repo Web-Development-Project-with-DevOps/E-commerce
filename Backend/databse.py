5753532bd6f5bae89055729011cd8670ee70e309
kumbharprath <142964670+kumbharprath@users.noreply.github.com>
2024-08-01 22:13:05 +0530

    Update databse.py

--- Backend/databse.py
+++ Backend/databse.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://prath123:prath3132@mycluster1.ritxzre.mongodb.net/")
+client = MongoClient("mongodb+srv://username:password@mycluster1.ritxzre.mongodb.net/")
 
 db = client[customerdata]
